471f369e5fcdf0f2282cf424abbe08af8eb7af0b
abdellahhn <abdellahhanane44@gmail.com>
2023-09-20 08:32:37 -0400

    hovered

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ for (let i = 0; i < maxSquares; i++) {
     square.classList.add('square');
     container.appendChild(square);
 }
+
+// we add the function to handle the listening
+function handleMouseEnter(event){
+    event.target.classList.add('hovered');
+}
+
+function handleMouseLeave(event) {
+    event.target.classList.remove('hovered');
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) =>{
+    square.addEventListener('mouseenter', handleMouseEnter);
+    square.addEventListener('mouseleave', handleMouseLeave);
+})
